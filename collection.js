9326cf013f867505bc3a2c0e887763c5e388223a
Mathieu Dutour <mathieu@dutour.me>
2017-05-23 09:51:09 +0200

    add document proxy

--- collection.js
+++ collection.js
@@ -1,7 +1,12 @@
 const applyMiddleware = require('./applyMiddlewares')
+const Document = require('./document')
 
 const Collection = function (zealot, name, options) {
   let _dispatch
+
+  const nativeCall = (args, {method, nativeCollection}) => {
+    return nativeCollection[method](...args)
+  }
 
   const collectionProxy = new Proxy({name, options}, {
     get (target, property, receiver) {
@@ -14,11 +19,30 @@ const Collection = function (zealot, name, options) {
         }
         case 'name':
           return name
+        case 'options':
+          return options
         default:
-          return (...argumentsList) => _dispatch((args, {method, nativeCollection}) => {
-            return nativeCollection[method](...args)
-          })(argumentsList, {method: property, collection: collectionProxy})
+          // handle `zealot.users[_id]`
+          if (zealot.id.isValid(property)) {
+            return new Document(zealot, collectionProxy, property)
+          }
+          // handle `zealot.users.find()`
+          return (...argumentsList) => _dispatch(nativeCall)(argumentsList, {method: property, collection: collectionProxy})
       }
+    },
+    // handle `delete zealot.users[_id]`
+    deleteProperty (target, property) {
+      if (zealot.id.isValid(property)) {
+        return _dispatch(nativeCall)([{_id: property}], {method: 'deleteOne', collection: collectionProxy})
+      }
+      return delete target[property]
+    },
+    // handle `zealot.users[_id] = something`
+    set (target, property, value, receiver) {
+      if (zealot.id.isValid(property)) {
+        return _dispatch(nativeCall)([{_id: property}, value], {method: 'update', collection: collectionProxy})
+      }
+      return target[property] = value // eslint-disable-line
     }
   })
 
